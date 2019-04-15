7f3bf5ee6bc8470442836c3d72df09b94b4d1d4a
Anton Kechashin <atk@sitecore.net>
2019-04-15 18:20:36 +0300

    Fix for SXA 1.7.1

--- src/Sitecore.Support.324927/ExperienceEditor/Speak/Ribbon/Requests/DatasourceUsages/GetDatasourceUsagesWithLockedStatus.cs
+++ src/Sitecore.Support.324927/ExperienceEditor/Speak/Ribbon/Requests/DatasourceUsages/GetDatasourceUsagesWithLockedStatus.cs
@@ -5,6 +5,10 @@
   using Sitecore.Diagnostics;
   using Sitecore.ExperienceEditor.Utils;
   using Sitecore.Links;
+  using Sitecore.XA.Foundation.SitecoreExtensions.Extensions;
+  using Sitecore.XA.Feature.Composites;
+  using System.Collections.Generic;
+  using System.Linq;
 
   public class GetDatasourceUsagesWithLockedStatus : Sitecore.ExperienceEditor.Speak.Ribbon.Requests.DatasourceUsages.GetDatasourceUsagesWithLockedStatus
   {
@@ -16,6 +20,26 @@
       LinksValidationResult finalLinksResult = new LinksValidationResult(layoutField.InnerField, ItemLinkState.Valid);
       //validation of the Links fills out the Links of the result;
       layoutField.ValidateLinks(finalLinksResult);
+      #region Added code
+      List<ItemLink> compositeSources = new List<ItemLink>();
+      foreach (ItemLink link in finalLinksResult.Links.Where(l => l.TargetItemID != item.ID))
+      {
+        Item linkedItem = Sitecore.Context.Database.GetItem(link.TargetItemID);
+        if (linkedItem.InheritsFrom(Templates.CompositeGroup.ID)) // for regular Composite
+        {
+          foreach (Item child in linkedItem.Children)
+          {
+            compositeSources.Add(new ItemLink (linkedItem, Sitecore.FieldIDs.FinalLayoutField, child, child.Paths.Path));
+            compositeSources.AddRange(this.GetItemLinks(child));
+          }
+        }
+        else if (linkedItem.InheritsFrom(Templates.CompositeSection.ID)) //for Snippet
+        {
+          compositeSources.AddRange(this.GetItemLinks(linkedItem));
+        }
+      }
+      finalLinksResult.Links.AddRange(compositeSources);
+      #endregion
       return finalLinksResult.Links.ToArray();
     }
   }
---
+++ src/Sitecore.Support.324927/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.324927")]
+[assembly: AssemblyProduct("Sitecore.Support.324927")]
+[assembly: ComVisible(false)]
